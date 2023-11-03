b0e164f0ca9c41a12e7f62a4414575b2aec5da11
Shoyeb45 <shoyebff45@gmail.com>
2023-11-03 21:58:59 +0530

    added two files

--- Questions/divisibleBy5&10.c
+++ Questions/divisibleBy5&10.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 
-void main()
+int main()
 {
     printf("Enter number a:");
     int a;
     scanf("%d",&a);
 
     if( (a % 5 == 0)  &&  (a % 10 == 0) ){
-        printf("The number %d is divisible by both 5 and 10",a);
+        printf("The number %d is divisible by both 5 and 10\n",a);
     } else{
-        printf("The number %d is not divisible by both 5 and 10", a);
+        printf("The number %d is not divisible by both 5 and 10\n", a);
     }
 
-    if ( a % 5 == 0)
-    printf("The number is divisible by 5");
-    if( a % 10 == 0)
-    printf("The number is divisible by 10");
+    if ( a % 5 == 0){
+    printf("The number %d is divisible by 5\n",a);
+    }
+    if( a % 10 == 0){
+    printf("The number %d is divisible by 10",a);
+    }
 }
