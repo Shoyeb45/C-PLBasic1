7f06864c45096c90a6204c079c722d85b238547a
Shoyeb45 <shoyebff45@gmail.com>
2023-11-16 00:05:14 +0530

    fixed mistake

--- Notes/forLoop.c
+++ Notes/forLoop.c
@@ -6,9 +6,9 @@ void main()
 	printf("Enter number till you want odd numbers:");
 	scanf("%d", & n);
 	
-	for ( int i = 1 ; i <= n ; n+=2){
-		printf("%d\n", i);
+	for ( int i=1 ; i<=n ; i+=2){
+		printf("%d ", i);
 	}
-		printf(" ");
+	
 	
 }
